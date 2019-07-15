7b57c95aa500c192356fb0dcbd6cccca3e119574
92959366ZIED <hhamdiziyed990@gmail.com>
2019-07-15 14:03:56 +0200

    first commit

---
+++ src/form.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './form.css'
+
+const CreditCard = (props) => {
+    return (
+       <div className="cardGlobale" > 
+        <div className="card">
+            <div className="companyName">
+                <h3>Company Name</h3>
+            </div>
+            <div className="cardPuce">
+                <img src={require('./puce.png')} alt="logo puce" />
+            </div>
+            <div className="cardNumber">
+                <p>{props.cardNumber ? props.cardNumber : '•••• •••• •••• ••••'}</p>
+            </div>
+            <div className="d-flex  bottomSection">
+                <div className=" right-bottomSection">
+                    <p className="date">{props.date ? props.date : '••/••'}</p>
+                    <h5 className="holderName">{props.holderName ? props.holderName : '••••••••••••••••••••'}</h5>
+                </div>
+            </div>
+
+        </div>
+      </div>  
+    )
+}
+export default CreditCard
